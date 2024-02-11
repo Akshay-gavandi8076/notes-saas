998004be1f57934efbcd6eabf13e4b25052df4f3
Akshay-gavandi8076 <Akshay.gavandi1@gmail.com>
2024-02-11 01:21:31 +0100

    feat: Added billing page and stripe webhook setup

--- src/components/SubmitButtons.tsx
+++ src/components/SubmitButtons.tsx
@@ -27,3 +27,51 @@ export function SubmitButton() {
     </>
   )
 }
+
+export function StripeSubscriptionCreationButton() {
+  const { pending } = useFormStatus()
+
+  return (
+    <>
+      {pending ? (
+        <Button
+          disabled
+          className='w-full'
+        >
+          <Loader2 className='mr-2 w-4 animate-spin' /> Please wait
+        </Button>
+      ) : (
+        <Button
+          className='w-full'
+          type='submit'
+        >
+          Create Subscription
+        </Button>
+      )}
+    </>
+  )
+}
+
+export function StripePortal() {
+  const { pending } = useFormStatus()
+
+  return (
+    <>
+      {pending ? (
+        <Button
+          disabled
+          className='w-fit'
+        >
+          <Loader2 className='mr-2 w-4 animate-spin' /> Please wait
+        </Button>
+      ) : (
+        <Button
+          className='w-fit'
+          type='submit'
+        >
+          View payment details
+        </Button>
+      )}
+    </>
+  )
+}
